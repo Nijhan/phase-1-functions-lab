bfda69733b1ad03976ea4b0715ed9e92c15bb2f0
Nijhan <najmashaaban6@gmail.com>
2025-06-28 17:37:19 +0300

    functions

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-function distanceFromHqInBlocks(pickupLocation){
-    const headquaters = 42;
-    return Math.abs(pickupLocation - 42);
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(someValue - 42);
 }
-function distanceFromHqInFeet(pickupLocation){
-    return distanceFromHqInBlocks(pickupLocation) *264;
-}
-function distanceTravelledInFeet(start, destination){
-    return Math.abs(destination - tsart) *264;
-}
-function calculatesFarePrice(start, destination){
-    const distance =distanceTravelledInFeet(start, destination);
 
-  if(distance<=400){
+function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
     return 0;
-   } else if (distance>400&&distance<=2000){
-    return (distance -400)*0.02;
-   }else if(distance>2000 && distance<=2500){
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
     return 25;
-   }else{
-return 'cannot travel that far';
-   }
+  } else {
+    return 'cannot travel that far';
   }
-
-console.log(distanceFromHqInBlocks(50))
-console.log(distanceFromHqInFeet(50))
-console.log(distanceTravelledInFeet(34, 38));
-console.log(distanceTravelledInFeet(38, 34));
+}
